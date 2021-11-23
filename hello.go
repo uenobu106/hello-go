f63ef8cb9324448a985dce7aba6e4ada0d89823b
uenobu106 <uenobu106@gmail.com>
2021-11-23 15:19:39 +0900

    named return values and naked return

--- hello.go
+++ hello.go
@@ -5,11 +5,24 @@ import (
 	"math"
 )
 
-func add(x int, y int) int {
+func add(x, y int) int {
 	return x + y
-} 
+}
+
+func swap(x, y string) (string, string){
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
 
 func main() {
 	fmt.Println(math.Pi)
 	fmt.Println(add(6, 4))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(split(19))
 }
