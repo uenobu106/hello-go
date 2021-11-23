3f9d7a07c11520d9b60e6962c87defbb759b313e
uenobu106 <uenobu106@gmail.com>
2021-11-23 15:45:42 +0900

    Short variable declarations

--- hello.go
+++ hello.go
@@ -19,10 +19,20 @@ func split(sum int) (x, y int) {
 	return
 }
 
+var c, python, java = true, false, "no"
+var m, j int = 1, 2
+
 func main() {
 	fmt.Println(math.Pi)
 	fmt.Println(add(6, 4))
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
 	fmt.Println(split(19))
+	var i int
+	fmt.Println(i, c, python, java)
+	fmt.Println(m, j)
+
+	var hoge, fuga string = "bar", "foo"
+	hogehoge := 456
+	fmt.Println(hoge, fuga, hogehoge)
 }
