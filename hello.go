405c7b421fa0eeefc8574c06ca89b4e201eb5dde
uenobu106 <uenobu106@gmail.com>
2021-11-24 20:04:59 +0900

    Exercise: Loops and Functions

--- hello.go
+++ hello.go
@@ -5,34 +5,20 @@ import (
 	"math"
 )
 
-func add(x, y int) int {
-	return x + y
+func Sqrt(x float64) float64 {
+	z := 1.0
+	a := 0.0
+	for i := 0; i < 10; i++ {
+		a = z
+		z -= (z*z - x) / (2*z)
+		fmt.Println(z)
+		if math.Abs(z - a) < 0.000001 {
+			break
+		}
+	}
+	return z
 }
-
-func swap(x, y string) (string, string){
-	return y, x
-}
-
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
-
-var c, python, java = true, false, "no"
-var m, j int = 1, 2
 
 func main() {
-	fmt.Println(math.Pi)
-	fmt.Println(add(6, 4))
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
-	fmt.Println(split(19))
-	var i int
-	fmt.Println(i, c, python, java)
-	fmt.Println(m, j)
-
-	var hoge, fuga string = "bar", "foo"
-	hogehoge := 456
-	fmt.Println(hoge, fuga, hogehoge)
+	fmt.Println(Sqrt(2))
 }
