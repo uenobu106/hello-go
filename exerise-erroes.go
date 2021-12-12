623f9a3684fbe7052a413ce6a540299315749f04
uenobu106 <uenobu106@gmail.com>
2021-12-12 17:16:45 +0900

    errors

---
+++ exerise-erroes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const delta = 0.00001
+
+
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return x, ErrNegativeSqrt(x)
+	}
+	z := x
+	n := 0.0
+	for math.Abs(n-z)> delta {
+		n, z = z, z-(z*z-x)/(2*z)
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
