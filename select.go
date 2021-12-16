2055e745df22d3784263374505bad5c775c3bb19
uenobu106 <uenobu106@gmail.com>
2021-12-16 22:41:32 +0900

    ✅ Select

---
+++ select.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c) // 10回受信
+		}
+		quit <- 0 //処理が終わったらquitへ送信
+	}()
+	fibonacci(c, quit)
+}
